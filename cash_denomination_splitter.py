a6b3e86107cac191ab4fc9c978dca8c3a296b861
djumanov <djumanovdev@gmail.com>
2025-05-23 10:37:13 +0500

    task01 bajarildi

--- cash_denomination_splitter.py
+++ cash_denomination_splitter.py
@@ -1,1 +1,17 @@
+amount = int(input("Pul miqdorini kiriting ($): "))
 
+k_50 = amount // 50
+amount = amount % 50
+print("$50 kupyuradan:", k_50, "ta")
+
+k_10 = amount // 10
+amount = amount % 10
+print("$10 kupyuradan:", k_10, "ta")
+
+k_5 = amount // 5
+amount = amount % 5
+print("$5 kupyuradan:", k_5, "ta")
+
+k_1 = amount // 1
+amount = amount % 1
+print("$1 kupyuradan:", k_1, "ta")
