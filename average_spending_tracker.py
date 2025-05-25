31e5756a822769ef9de08e9f71ed7b5c3be2227b
xolmanovsardor <sardorxolmanov2@gmail.com>
2025-05-25 20:04:45 +0300

    hammasi bajarildi

--- average_spending_tracker.py
+++ average_spending_tracker.py
@@ -1,1 +1,10 @@
+from desemal import Desemal 
 
+a = Desemal("5")
+b = Desemal("8")
+c = Desemal("3")
+d = Desemal("73")
+f = Desemal("59")
+g = Desemal("47")
+e = Desemal("26")
+print (a + b + c + d + e + f + g)
